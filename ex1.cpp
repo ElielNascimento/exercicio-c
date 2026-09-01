de3b4eaed2b8a420535cd76e030b12d35b3fc8a2
unknown <nascimentob.eliel@gmail.com>
2026-09-01 08:41:15 -0300

    palmeiras nao tem mundial

---
+++ ex1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+char letra;
+
+int main (){
+	
+	printf("Digite um letra ");
+	scanf("%c", &letra);
+	
+	printf("A letra que voce digitou foi : %c", letra);
+	
+	return 0;
+}
+
